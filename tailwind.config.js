2ad72a4bb1aec99ed17363d8aad0dc2d8b69988e
unknown <dhinarnm@gmail.com>
2024-10-02 21:11:57 +0700

    pertama

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#0D63A5',
+        secondary:'#FFFFFF',
+        tertiary: '#FFD717', 
+
+      }
+    },
+  },
+  plugins: [],
+}
+
